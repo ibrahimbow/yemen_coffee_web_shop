68bc65f7d07d58e61a6d3c661a7786ea306419ae
ibrahimbow <smartman402@hotmail.com>
2020-07-23 00:11:06 +0200

    Yemen coffee web shop springboot

---
+++ src/main/java/intecbrussel/yemencoffee_webshop/YemenCoffeeWebShopApplication.java
@@ -0,0 +1,14 @@
+package intecbrussel.yemencoffee_webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemenCoffeeWebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YemenCoffeeWebShopApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/intecbrussel/yemencoffee_webshop/YemencoffeeWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package intecbrussel.yemencoffee_webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemencoffeeWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
