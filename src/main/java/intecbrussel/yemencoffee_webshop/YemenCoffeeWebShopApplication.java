41ccb2389c2c7b17ea3aded4f39117ce5f0e19b4
ibrahimbow <smartman402@hotmail.com>
2020-07-23 00:53:34 +0200

    add template html pages

--- src/main/java/intecbrussel/yemencoffee_webshop/YemenCoffeeWebShopApplication.java
+++ src/main/java/intecbrussel/yemencoffee_webshop/YemenCoffeeWebShopApplication.java
@@ -2,13 +2,20 @@ package intecbrussel.yemencoffee_webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class YemenCoffeeWebShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YemenCoffeeWebShopApplication.class, args);
+
+
+        ConfigurableApplicationContext applicationYemenCoffeeWebShop
+                = SpringApplication.run(YemenCoffeeWebShopApplication.class, args);
+
+
+
+
+
     }
-
-
 }
